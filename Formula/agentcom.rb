828aafeaa27ffce1419e23265e05ac71b79303d8
Junghun Yang(malleus) <36567887+malleus35@users.noreply.github.com>
2026-03-17 23:37:48 +0900

    chore: update agentcom formula for v0.3.0

--- Formula/agentcom.rb
+++ Formula/agentcom.rb
@@ -6,22 +6,22 @@ class Agentcom < Formula
   on_macos do
     on_arm do
       url "https://github.com/malleus35/agentcom/releases/download/v0.3.0/agentcom_0.3.0_darwin_arm64.tar.gz"
-      sha256 "4bd79ac195f6480d6add9c5d68c2779a35cde9947be98f9572e28ed831a8e5ec"
+      sha256 "fd2b1454dcc722d609c482df276dbc5f729711c1b569bf515ed5dfcf1f7d531e"
     end
     on_intel do
       url "https://github.com/malleus35/agentcom/releases/download/v0.3.0/agentcom_0.3.0_darwin_amd64.tar.gz"
-      sha256 "5d36722f54106e7e0d7422d7ac83ab190936ec8bc66d2eeb4525a45a02bcb72d"
+      sha256 "27bb0bda39a472f0fbee13190979debdab8003611e7c8679bf5eeb38a916ab88"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/malleus35/agentcom/releases/download/v0.3.0/agentcom_0.3.0_linux_arm64.tar.gz"
-      sha256 "64a0532b4f6ff0b89b7e6de9c4241fa88cd9e006ef4a88110d899098caae1ff9"
+      sha256 "0d921968530939318c01de1e25041031138d76919ffec4d93aa530e856829927"
     end
     on_intel do
       url "https://github.com/malleus35/agentcom/releases/download/v0.3.0/agentcom_0.3.0_linux_amd64.tar.gz"
-      sha256 "2cada0a6bddb72484a86ca5007547106d1032663a2e6a68d29bf772e3dd63441"
+      sha256 "f3970f3280924b3f231f130553bc3caf1db1f80d552420173952142094b675ac"
     end
   end
 
