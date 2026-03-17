d95ad9742849e97681731f206af70ce2477265e3
malleus <36567887+malleus35@users.noreply.github.com>
2026-03-17 15:46:12 +0900

    chore: update agentcom formula for v0.2.3

--- Formula/agentcom.rb
+++ Formula/agentcom.rb
@@ -1,27 +1,27 @@
 class Agentcom < Formula
   desc "Real-time communication CLI for parallel AI coding agents"
   homepage "https://github.com/malleus35/agentcom"
-  version "0.2.2"
+  version "0.2.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/malleus35/agentcom/releases/download/v0.2.2/agentcom_0.2.2_darwin_arm64.tar.gz"
-      sha256 "14c7d917bf3bf053bfe53cb3dc321bd6931ca7b3c1176de96842c2effcb9c8ff"
+      url "https://github.com/malleus35/agentcom/releases/download/v0.2.3/agentcom_0.2.3_darwin_arm64.tar.gz"
+      sha256 "02640b53105c77efdcb4330774b6720cd5a6924f4ea6ac02b0b8bcd35a1ef664"
     end
     on_intel do
-      url "https://github.com/malleus35/agentcom/releases/download/v0.2.2/agentcom_0.2.2_darwin_amd64.tar.gz"
-      sha256 "5275016b64e7b7aeb6e02b644cdc3e68b249b99b84801f993909b01f9f73f6f7"
+      url "https://github.com/malleus35/agentcom/releases/download/v0.2.3/agentcom_0.2.3_darwin_amd64.tar.gz"
+      sha256 "a6b5ec58772965211b684214cb40c224b04b75c95f83084fb5b44910107bdf55"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/malleus35/agentcom/releases/download/v0.2.2/agentcom_0.2.2_linux_arm64.tar.gz"
-      sha256 "b2ac1d5250219ee713c0dd09793f9581221e6d00cc48e37c4a2fcc77ec12a126"
+      url "https://github.com/malleus35/agentcom/releases/download/v0.2.3/agentcom_0.2.3_linux_arm64.tar.gz"
+      sha256 "44381c37cbada97e3b1f86cc61dc8f50033d8f4b1970ce45796749dfb8208b6a"
     end
     on_intel do
-      url "https://github.com/malleus35/agentcom/releases/download/v0.2.2/agentcom_0.2.2_linux_amd64.tar.gz"
-      sha256 "ef6fe217cee145a023626003b89535817dca9f0f56697233fd0e038bec7fc7e9"
+      url "https://github.com/malleus35/agentcom/releases/download/v0.2.3/agentcom_0.2.3_linux_amd64.tar.gz"
+      sha256 "c55c6df9cc83b3f637698c89fa712e6c11e007c3d1a7da02cf069e7016537885"
     end
   end
 
@@ -30,6 +30,6 @@ class Agentcom < Formula
   end
 
   test do
-    assert_match "agentcom", shell_output("#{bin}/agentcom version")
+    assert_match "agentcom", shell_output("#<built-in function bin>/agentcom version")
   end
 end
